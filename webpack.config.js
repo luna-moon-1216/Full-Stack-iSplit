e9b7df7c45e5df4f65ad424ff6f7b1322a07bec7
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-10-11 22:08:43 -0400

    init

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;
